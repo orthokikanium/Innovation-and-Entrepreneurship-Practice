24d1e588af28f0ab46f36f305979e0e98122b6e8
Hyan1ce <109321680+Hyan1ce@users.noreply.github.com>
2022-07-30 18:13:35 +0800

    Delete Bitcoin_public_send_tx.py

--- Bitcoin_public_send_tx.py
+++
@@ -1,5 +0,0 @@
-from requests_html import HTMLSession
-session = HTMLSession()
-r= session.get('https://www.blockchain.com/btc/tx/4645603184e112828bf9a52275deb01e501635589ea02af8e3c4e12fbff9e16c')
-print(dir(r.html))
-
